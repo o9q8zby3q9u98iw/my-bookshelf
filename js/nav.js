418df0125fde8cb69f9c44b1b3c81a6e46b68e9c
o9q8zby3q9u98iw <brkannualmeeting@gmail.com>
2026-03-01 10:45:58 -0500

    Create nav.js

---
+++ js/nav.js
@@ -0,0 +1,16 @@
+class SiteNav extends HTMLElement {
+    connectedCallback() {
+        const activePage = this.getAttribute('active-page');
+        
+        this.innerHTML = `
+            <nav class="top-nav">
+                <div class="nav-links">
+                    <a href="index.html" class="${activePage === 'home' ? 'active' : ''}">Home</a>
+                    <a href="bookshelf.html" class="${activePage === 'bookshelf' ? 'active' : ''}">Bookshelf</a>
+                    <a href="https://www.linkedin.com/in/cmhershey" target="_blank">LinkedIn</a>
+                </div>
+            </nav>
+        `;
+    }
+}
+customElements.define('site-nav', SiteNav);
